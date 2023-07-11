5cfb2a6d6fac967c1fd5985b52faf601d5714647
dewaldvdw <dewaldv@vivaldi.net>
2023-07-11 19:28:29 +0200

    Online CV

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('W3.CSS Template.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
